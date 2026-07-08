1a2a357b2a324d1d6151216b88a1adc87a3bd390
rayanedami <158374987+rayanedami@users.noreply.github.com>
2026-07-08 18:36:55 +0100

    Initial commit of Treasure Shuffle project

---
+++ script.js
@@ -0,0 +1,8 @@
+const startButton = document.getElementById("startButton");
+
+startButton.addEventListener("click", () => {
+
+    alert("Game Starts!");
+
+    // Ici nous afficherons la page du jeu.
+});
